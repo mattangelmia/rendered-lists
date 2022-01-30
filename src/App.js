f477217a43ace6ada6000404be8b99b77b7849aa
matt angel <23matthewangel@gmail.com>
2022-01-30 00:20:23 -0500

    added total

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import List from './List'
+import Total from './Total'
 
 function App() {
+const [total, setTotal] = useState(0)
+
+useEffect(()=>{
+  
+    let exercises = course.parts.map(part=>part.exercises)
+    let totalExercises = exercises.reduce(function(a, b) { return a + b; }, 0);
+    console.log(totalExercises)
+    setTotal(totalExercises);
+  
+
+},[])
+
   const course = {
     id: 1,
     name: 'Half Stack application development',
@@ -22,13 +36,24 @@ function App() {
         exercises: 14,
         id: 3
       }
+
+      ,
+      {
+        name: 'Redux',
+        exercises:11,
+        id: 4
+      }
+
+
     ]
   }
+ 
 
 
   return (
     <div className="App">
     <List course={course.parts} header={course.name}/>
+    <Total total={total}/>
     </div>
   );
 }
---
+++ src/Total.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Total(props) {
+  return <div>
+      <h4>Total of {props.total} exercises</h4>
+  </div>;
+}
